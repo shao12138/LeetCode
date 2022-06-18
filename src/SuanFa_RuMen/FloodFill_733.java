d1a56d417c380262ecc8a877ce149f7598f851b7
shao12138 <1966799809@qq.com>
2022-06-18 22:20:49 +0800

    ss

--- src/SuanFa_RuMen/FloodFill_733.java
+++ src/SuanFa_RuMen/FloodFill_733.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class FloodFill_733 {
-     static int[] dx = {1, 0, 0, -1};
+    static int[] dx = {1, 0, 0, -1};
     static int[] dy = {0, 1, -1, 0};
 
     public static void main(String[] args) {
