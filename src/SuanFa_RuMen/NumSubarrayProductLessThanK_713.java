4e846c25fba79af2615b357caf5ebb1fd243d858
shao12138 <1966799809@qq.com>
2022-06-27 23:22:09 +0800

    ss

--- src/SuanFa_RuMen/NumSubarrayProductLessThanK_713.java
+++ src/SuanFa_RuMen/NumSubarrayProductLessThanK_713.java
@@ -2,26 +2,27 @@ package SuanFa_RuMen;
 
 public class NumSubarrayProductLessThanK_713 {
     public static void main(String[] args) {
-        int[] a = new int[]{1,1,1};
-        int k = 2;
+        int[] a = new int[]{10,5,2,6};
+        int k = 100;
         System.out.println(numSubarrayProductLessThanK(a, k));
     }
 
     public static int numSubarrayProductLessThanK(int[] nums, int k) {
-        int length = 1;
-        int result = 0;
-        while (length < nums.length) {
-            for (int i = 0; i < nums.length; i++) {
-                int sum = nums[i];
-                for (int j = i + 1; j < length; j++) {
-                    sum *= nums[j];
-                }
-                if (sum < k) {
-                    result++;
-                }
+        // 滑动窗口：寻找以每个 right 指针为右边界的有效连续子树组的个数
+        int length = nums.length;
+        int product = 1;
+        int cnt = 0;
+        int left = 0, right = 0;
+        while (right < length) {
+            product *= nums[right++];
+
+            while (left < right && product >= k) {
+                product /= nums[left++];
             }
-            length++;
+
+            cnt += (right - left);
         }
-        return result;
+
+        return cnt;
     }
 }
