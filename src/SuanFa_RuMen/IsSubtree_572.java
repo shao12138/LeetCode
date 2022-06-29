c9b3cbbbdc1478d63c18074bcbc999819ec47951
shao12138 <1966799809@qq.com>
2022-06-29 23:53:06 +0800

    ss

---
+++ src/SuanFa_RuMen/IsSubtree_572.java
@@ -0,0 +1,27 @@
+package SuanFa_RuMen;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class IsSubtree_572 {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return dfs(s, t);
+    }
+
+    public boolean dfs(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        return check(s, t) || dfs(s.left, t) || dfs(s.right, t);
+    }
+
+    public boolean check(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null || s.val != t.val) {
+            return false;
+        }
+        return check(s.left, t.left) && check(s.right, t.right);
+    }
+}
