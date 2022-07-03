6bf8fd6b019c9aaba405f04983a95f810fbd5003
shao12138 <1966799809@qq.com>
2022-07-03 23:47:22 +0800

    ss

---
+++ src/SuanFa_RuMen/MaxSubArray_53.java
@@ -0,0 +1,12 @@
+package SuanFa_RuMen;
+
+public class MaxSubArray_53 {
+    public int maxSubArray(int[] nums) {
+        int m = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+            m = Math.max(m, nums[i]);
+        }
+        return m;
+    }
+}
