e7b46448f1344ea0d222890d00f099460459bdf2
shao12138 <1966799809@qq.com>
2022-07-04 23:15:15 +0800

    ss

---
+++ src/SuanFa_RuMen/CanJump_55.java
@@ -0,0 +1,23 @@
+package SuanFa_RuMen;
+
+public class CanJump_55 {
+    public boolean canJump(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                count++;
+            }
+        }
+        if (count == nums.length) {
+            return true;
+        }
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                return false;
+            }
+            k = Math.max(k, i + nums[i]);
+        }
+        return true;
+    }
+}
