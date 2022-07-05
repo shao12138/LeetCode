362dc5cdd3f8f08284c469223ee7acbe239dbb46
shao12138 <1966799809@qq.com>
2022-07-05 23:42:16 +0800

    ss

--- src/SuanFa_RuMen/Jump_45.java
+++ src/SuanFa_RuMen/Jump_45.java
@@ -2,13 +2,12 @@ package SuanFa_RuMen;
 
 public class Jump_45 {
     public static void main(String[] args) {
-        int[] a = new int[]{2, 3, 1, 1, 4, 4};
+        int[] a = new int[]{2, 3, 1, 1, 4,4};
         jump(a);
     }
 
     public static int jump(int[] nums) {
         int count = 0;
-        int k = 0;
         for (int i = 0; i < nums.length; ) {
             int max = Integer.MIN_VALUE;
             for (int j = 0; j < nums[i]; j++) {
