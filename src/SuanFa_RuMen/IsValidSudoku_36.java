bf4aefe62df02ec984bb1fdb78bd63833b5622b8
shao12138 <1966799809@qq.com>
2022-07-27 07:43:47 +0800

    ss

---
+++ src/SuanFa_RuMen/IsValidSudoku_36.java
@@ -0,0 +1,47 @@
+package SuanFa_RuMen;
+
+public class IsValidSudoku_36 {
+    public static void main(String[] args) {
+        char[][] c = new char[][]{
+                {'.', '.', '.', '.', '.', '.', '5', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'9', '3', '.', '.', '2', '.', '4', '.', '.'},
+                {'.', '.', '7', '.', '.', '.', '3', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '3', '4', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '3', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '5', '2', '.', '.'}
+        };
+        isValidSudoku(c);
+    }
+
+    public static boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    for (int l = 0; l < 9; l++) {
+                        if (board[i][l] == board[i][j] && l != j) {
+                            return false;
+                        }
+                    }
+                    for (int r = 0; r < 9; r++) {
+                        if (board[r][j] == board[i][j] && i != r) {
+                            return false;
+                        }
+                    }
+                    int start1 = i / 3;
+                    int start2 = j / 3;
+                    for (int m = start1 * 3; m < start1 * 3 + 3; m++) {
+                        for (int n = start2 * 3; n < start2 * 3 + 3; n++) {
+                            if (board[m][n] == board[i][j] && m != i && n != j) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
