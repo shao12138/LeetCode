25800f14b38242c5a20d28f3840df76ae84563af
shao12138 <1966799809@qq.com>
2022-08-01 11:01:20 +0800

    ss

---
+++ src/SuanFa_RuMen/InorderTraversal_94.java
@@ -0,0 +1,32 @@
+package SuanFa_RuMen;
+
+import java.util.*;
+
+public class InorderTraversal_94 {
+    static List a = new ArrayList();
+    static Stack<TreeNode> stack = new Stack();
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                a.add(root.val);
+                root = root.right;
+            }
+
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        a.right = b;
+        b.left = c;
+        inorderTraversal(a);
+    }
+}
