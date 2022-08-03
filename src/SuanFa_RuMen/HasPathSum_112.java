e266ce4d8d38556c9e0d0bd229488b29ea2fcc11
shao12138 <1966799809@qq.com>
2022-08-03 11:45:15 +0800

    ss

---
+++ src/SuanFa_RuMen/HasPathSum_112.java
@@ -0,0 +1,34 @@
+package SuanFa_RuMen;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class HasPathSum_112 {
+
+    boolean ans = false;
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        dfs(root, sum);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, int sum) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.left == null && root.right == null) {
+            if (sum == root.val) {
+                ans = true;
+            }
+        }
+
+        dfs(root.left, sum - root.val);
+        dfs(root.right, sum - root.val);
+    }
+}
