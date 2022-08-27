98cb1218769b8856388774e8b42fe605a8a66b6e
shao12138 <1966799809@qq.com>
2022-08-27 09:33:07 +0800

    ss

---
+++ src/SuanFa_other/SubtractProductAndSum_1281.java
@@ -0,0 +1,28 @@
+package SuanFa_other;
+
+public class SubtractProductAndSum_1281 {
+    public static void main(String[] args) {
+        int n = 705;
+        System.out.println(subtractProductAndSum(n));
+    }
+
+    public static int subtractProductAndSum(int n) {
+        if (n == 100000) {
+            return n;
+        }
+        int length = (n + "").length();
+        int num[] = new int[5];
+        for (int i = 1; i <= 5; i++) {
+            num[i - 1] = (int) ((n % Math.pow(10, i)) / Math.pow(10, i - 1));
+        }
+        int a = 1;
+        int b = 0;
+        for (int i = 0; i < 5; i++) {
+            if (i < length) {
+                a *= num[i];
+            }
+            b += num[i];
+        }
+        return a - b;
+    }
+}
