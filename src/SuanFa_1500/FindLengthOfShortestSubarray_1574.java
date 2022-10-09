f49aef38ea4e17ca7f8a5d963807cfcef7fef124
shao12138 <1966799809@qq.com>
2022-10-09 22:59:04 +0800

    ss

--- src/SuanFa_1500/FindLengthOfShortestSubarray_1574.java
+++ src/SuanFa_1500/FindLengthOfShortestSubarray_1574.java
@@ -1,41 +1,39 @@
 package SuanFa_1500;
 
-import java.util.Arrays;
-
 public class FindLengthOfShortestSubarray_1574 {
-    public static void main(String[] args) {
-        int[] arr = new int[]{13, 0, 14, 7, 18, 18, 18, 16, 8, 15, 20};
-        findLengthOfShortestSubarray(arr);
-    }
-
-    public static int findLengthOfShortestSubarray(int[] arr) {
-        int length = findNumberOfLIS(arr);
-        return arr.length - length;
-    }
-
-    public static int findNumberOfLIS(int[] nums) {
-        if (nums.length == 0) {
+    public int findLengthOfShortestSubarray(int[] arr) {
+        int n = arr.length;
+        int leftEnd = 0;
+        for (int i = 1; i < n && arr[i] >= arr[i - 1]; i++) {
+            leftEnd = i;
+        }
+        if (leftEnd == n - 1) {
             return 0;
         }
-        int[] dp = new int[nums.length];
-        int[] combination = new int[nums.length];
-        Arrays.fill(dp, 1);
-        Arrays.fill(combination, 1);
-        int max = 1, res = 0;
-
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] >= nums[j]) {
-                    if (dp[j] + 1 > dp[i]) { //如果+1长于当前LIS 则组合数不变
-                        dp[i] = dp[j] + 1;
-                        combination[i] = combination[j];
-                    } else if (dp[j] + 1 == dp[i]) { //如果+1等于当前LIS 则说明找到了新组合
-                        combination[i] += combination[j];
-                    }
-                }
-            }
-            max = Math.max(max, dp[i]);
+        int rightStart = n - 1;
+        for (int i = n - 2; i >= 0 && arr[i] <= arr[i + 1]; i--) {
+            rightStart = i;
         }
-        return max;
+        int low = rightStart - leftEnd - 1;
+        int high = Math.min(rightStart, n - leftEnd - 1);
+        while (low < high) {
+            int tmp = low + (high - low) / 2;
+            if (canMakeSorted(arr, leftEnd + 1, rightStart - 1, tmp)) {
+                high = tmp;
+            } else {
+                low = tmp + 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean canMakeSorted(int[] arr, int midStart, int midEnd, int remove) {
+        int n = arr.length;
+        for (int i = midEnd - remove, j = midEnd + 1; i < midStart; i++, j++) {
+            if (arr[i] <= arr[j]) {
+                return true;
+            }
+        }
+        return false;
     }
 }
