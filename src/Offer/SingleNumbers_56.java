4d1e707306f73e7276de9ae1eaa2ff2adafca95c
shao12138 <1966799809@qq.com>
2022-11-26 16:11:38 +0800

    ss

---
+++ src/Offer/SingleNumbers_56.java
@@ -0,0 +1,19 @@
+package Offer;
+
+public class SingleNumbers_56 {
+    public static void main(String[] args) {
+        int result = 0;
+        int[] nums = new int[]{1,2,10,4,1,4,3,3};
+        for (int i : nums) {
+            result ^= i;
+        }
+        System.out.println(result);
+    }
+
+    public int[] singleNumbers(int[] nums) {
+        for (int i : nums) {
+
+        }
+        return new int[]{};
+    }
+}
