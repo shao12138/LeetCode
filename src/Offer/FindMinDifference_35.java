da6e5cdf4de6a3785a5834e950bf6f64dbd4c4ea
shao12138 <1966799809@qq.com>
2022-12-21 13:48:32 +0800

    asd

---
+++ src/Offer/FindMinDifference_35.java
@@ -0,0 +1,45 @@
+package Offer;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class FindMinDifference_35 {
+    public static void main(String[] args) throws ParseException {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("01:39");
+        arrayList.add("10:26");
+        arrayList.add("21:43");
+        findMinDifference(arrayList);
+    }
+
+    public static int findMinDifference(List<String> timePoints) {
+        int min = Integer.MAX_VALUE;
+        try {
+            Object[] times = timePoints.toArray();
+            ArrayList<Long> result = new ArrayList();
+            Arrays.sort(times);
+            for (int i = 1; i < times.length; i++) {
+                Date date1 = new SimpleDateFormat("HH:mm").parse((String) times[i - 1]);
+                Date date2 = new SimpleDateFormat("HH:mm").parse((String) times[i]);
+                result.add((date2.getTime() - date1.getTime()) / (1000 * 60));
+            }
+            Date date0 = new SimpleDateFormat("HH:mm").parse((String) times[0]);
+            Date date00 = new SimpleDateFormat("HH:mm").parse((String) "00:00");
+            long f = date0.getTime() - date00.getTime();
+            Date date1 = new SimpleDateFormat("HH:mm").parse((String) "24:00");
+            Date date2 = new SimpleDateFormat("HH:mm").parse((String) times[times.length - 1]);
+            f += date1.getTime() - date2.getTime();
+            result.add(f / (1000 * 60));
+            for (int i = 0; i < result.size(); i++) {
+                min = (int) Math.min(min, result.get(i));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return min;
+    }
+}
---
+++ src/Offer/IsAlienSorted_34.java
@@ -0,0 +1,36 @@
+package Offer;
+
+public class IsAlienSorted_34 {
+    public static void main(String[] args) {
+        String[] str = new String[]{"word","world","row"};
+        String order = "worldabcefghijkmnpqstuvxyz";
+        isAlienSorted(str, order);
+    }
+
+    public static boolean isAlienSorted(String[] words, String order) {
+        for (int i = 0; i < words.length - 1; i++) {
+            String o1 = words[i];
+            String o2 = words[i + 1];
+            int count = 0;
+            for (int j = 0; j < o1.length() && j < o2.length(); j++) {
+                char c1 = o1.charAt(j);
+                char c2 = o2.charAt(j);
+                if (c1 != c2) {
+                    if (order.indexOf(c1) >= order.indexOf(c2)) {
+                        return false;
+                    }else {
+                        break;
+                    }
+                } else {
+                    count++;
+                }
+            }
+            if (count == Math.min(o1.length(), o2.length())) {
+                if (o1.length() > o2.length()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
