7a8f26abb14c7dc7bb12d7f7e70480e4208543b8
shao12138 <1966799809@qq.com>
2022-12-22 13:28:16 +0800

    asd

---
+++ src/Offer/AsteroidCollision_37.java
@@ -0,0 +1,33 @@
+package Offer;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class AsteroidCollision_37 {
+    public static void main(String[] args) {
+        int[] a = new int[]{10, 2, -5};
+        asteroidCollision(a);
+    }
+
+    public static int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        for (int aster : asteroids) {
+            boolean alive = true;
+            while (alive && aster < 0 && !stack.isEmpty() && stack.peek() > 0) {
+                alive = stack.peek() < -aster; // aster 是否存在
+                if (stack.peek() <= -aster) {  // 栈顶行星爆炸
+                    stack.pop();
+                }
+            }
+            if (alive) {
+                stack.push(aster);
+            }
+        }
+        int size = stack.size();
+        int[] ans = new int[size];
+        for (int i = size - 1; i >= 0; i--) {
+            ans[i] = stack.pop();
+        }
+        return ans;
+    }
+}
