c7e5fc2cf475605d5726608422b86c6f415b1f5f
shao12138 <1966799809@qq.com>
2023-03-08 08:34:42 +0800

    asd

---
+++ src/SuanFa_400/PacificAtlantic_417.java
@@ -0,0 +1,63 @@
+package SuanFa_400;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class PacificAtlantic_417 {
+    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int[][] heights;
+    int m, n;
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        this.heights = heights;
+        this.m = heights.length;
+        this.n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            bfs(i, 0, pacific);
+        }
+        for (int j = 1; j < n; j++) {
+            bfs(0, j, pacific);
+        }
+        for (int i = 0; i < m; i++) {
+            bfs(i, n - 1, atlantic);
+        }
+        for (int j = 0; j < n - 1; j++) {
+            bfs(m - 1, j, atlantic);
+        }
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    List<Integer> cell = new ArrayList<Integer>();
+                    cell.add(i);
+                    cell.add(j);
+                    result.add(cell);
+                }
+            }
+        }
+        return result;
+    }
+
+    public void bfs(int row, int col, boolean[][] ocean) {
+        if (ocean[row][col]) {
+            return;
+        }
+        ocean[row][col] = true;
+        Queue<int[]> queue = new ArrayDeque<int[]>();
+        queue.offer(new int[]{row, col});
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] dir : dirs) {
+                int newRow = cell[0] + dir[0], newCol = cell[1] + dir[1];
+                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && heights[newRow][newCol] >= heights[cell[0]][cell[1]] && !ocean[newRow][newCol]) {
+                    ocean[newRow][newCol] = true;
+                    queue.offer(new int[]{newRow, newCol});
+                }
+            }
+        }
+    }
+}
