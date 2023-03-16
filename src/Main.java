47e34d09bcac045aa02c80a950eae82848a38eeb
shao12138 <1966799809@qq.com>
2023-03-16 08:00:18 +0800

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int a = 1;
+        Integer b = new Integer(1);
+        System.out.println(a==b);
+        String c = "1";
+        String d = new String("1");
+        System.out.println(c==d);
     }
 }
