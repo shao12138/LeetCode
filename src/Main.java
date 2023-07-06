4f831247ebeb7e1b6dba54f0e4c060670af5223f
shao12138 <1966799809@qq.com>
2023-07-06 09:34:30 +0800

    444

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,8 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        int count = 1;
-        int row = 0, col = 0;
-        boolean down = true, right = false;
-        while (count <= n * n) {
-            arr[row][col] = count;
-            count++;
-            if (down) {
-                if (row == n - 1) {
-                    right = true;
-                    down = false;
-                    col++;
-                } else if (col == 0) {
-                    down = true;
-                    right = false;
-                    row++;
-                } else {
-                    row++;
-                    col--;
-                }
-            } else {
-                if (col == n - 1) {
-                    right = false;
-                    down = true;
-                    row++;
-                } else if (row == 0) {
-                    down = false;
-                    right = true;
-                    col++;
-                } else {
-                    row--;
-                    col++;
-                }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        String c = "ABCDEFG";
+        String d = "中华人民共和国";
+        System.out.println(c.length());
+        System.out.println(d.length());
     }
 }
